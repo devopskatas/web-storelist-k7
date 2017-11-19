39ccf7cf71de4425395db280bf2ee90cd0d31186
DevOps Katas <dave@devopskatas.com>
2017-11-19 12:18:54 -0600

    adding tests

---
+++ tests/listvalidator.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import os
+import sys
+
+masterList = [] 
+nextList = []
+with open(os.getcwd() + "./storelist.htm") as fp:
+    soup = BeautifulSoup(fp, "html.parser")
+    
+    
+    print("SECTIONS:")
+    # for content in soup.find_all("ul", {"id":"grocerySections"}):
+    #     if content.text:
+    #         print (content.text)
+    
+    #print("MASTERLIST:")
+    for content in soup.find_all("ol", {"id":"masterList"}):
+        if content.text:
+            masterList = content.text.splitlines()
+    
+    #print("NEXTVISIT:")
+    for content in soup.find_all("ol",{"id":"nextList"}):
+        if content.text:
+            nextList = content.text.splitlines()
+
+    #print("MISMATCH:")
+
+#print(len(masterList))
+#print(len(nextList))
+
+diff = list(set(nextList) - set(masterList))
+numDiffs = len(diff)
+
+print(diff)
+
+if numDiffs > 0:
+    print('LIST INVALID ' + str(len(diff)))
+    sys.exit(1)
+
+sys.exit(0)
